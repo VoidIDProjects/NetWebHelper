1a372de30009745117b64888f22cb30c541d73e2
Space VOID <void_id@protonmail.com>
2019-12-07 02:23:51 +0300

    work

--- WebHelper/MainForm.cs
+++ WebHelper/MainForm.cs
@@ -13,7 +13,7 @@ namespace WebHelper
 
         private void buttonURI_Click(object sender, EventArgs e)
         {
-            if (Clipboard.ContainsText()) return;
+            if (!Clipboard.ContainsText()) return;
             textBoxURI.Text = Clipboard.GetText();
         }
 
@@ -31,9 +31,25 @@ namespace WebHelper
             string uri = textBoxURI.Text;
             var headers = textBoxHeaders.Lines;
             var result = new StringBuilder();
+            string acLanguage = null;
+            string userAgent = null;
 
             result.Append("private IDVResponse getResponse()\n{\n");
+            result.Append($"IDVRequestData data = new IDVRequestData(\"{uri}\");\n");
+            foreach (var item in headers)
+            {
+                if (item.Contains("Host")) continue;
+                if (item.Contains("Connection")) continue;
+                if (item.Contains("Accept-Encoding")) continue;
+                if (item.Contains("Accept-Language")){ acLanguage = item.Split(' ')[1]; continue; }
+                if (item.Contains("User-Agent")) { userAgent = item.Split(' ')[1]; continue; }
+                result.Append($"data.Headers.Add(\"{item}\");\n");
+            }
+            if (!string.IsNullOrEmpty(acLanguage)) result.Append($"_client.AcceptLanguage = \"{acLanguage}\";\n");
+            if(!string.IsNullOrEmpty(userAgent)) result.Append($"_client.UserAgent = \"{userAgent}\";\n");
             result.Append("}");
+            Clipboard.Clear();
+            Clipboard.SetText(result.ToString());
         }
     }
 }
